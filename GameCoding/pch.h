9c78c7fec80fb6cac1ee810e3dfe9de4604d4d62
박제영 <josunyjostar@gmail.com>
2023-12-15 14:11:41 +0900

    build : precompile header setting

--- GameCoding/pch.h
+++ GameCoding/pch.h
@@ -1,1 +1,22 @@
 #pragma once
+#include "Types.h"
+#include "Values.h"
+#include "Struct.h"
+
+#include <vector>
+#include <list>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+#include <windows.h>
+#include <assert.h>
+
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+
+
+
